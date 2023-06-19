d07086ff2bf88bcec889fbf4b71c34268bc99493
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-06-19 16:28:54 +0200

    GymOwner is Null oplossen en daarna meer errors in terminal ontdekt

--- src/PokemonTrainer.java
+++ src/PokemonTrainer.java
@@ -6,16 +6,30 @@ public class PokemonTrainer {
     private String town;
     private List<Pokemon> pokemons = new ArrayList<>();
 
+    public PokemonTrainer(String name, String town, List<Pokemon> pokemons) {
+        this.name = name;
+        this.town = town;
+        this.pokemons = pokemons;
+    }
+
     public PokemonTrainer(String name) {
-        super();
+        this.name = name;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getTown() {
         return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
     }
 
     public List<Pokemon> getPokemons() {
